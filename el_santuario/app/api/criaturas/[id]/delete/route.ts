19d12092822e87be7889a44673b32e3a995e495f
Laura Cascallana <lauracascallanaferreira@gmail.com>
2025-06-17 12:05:23 +0200

    solucion problemas con delete

--- el_santuario/app/api/criaturas/[id]/delete/route.ts
+++ el_santuario/app/api/criaturas/[id]/delete/route.ts
@@ -3,29 +3,30 @@ import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-type Context = {
-  params: {
-    id: string;
-  };
-};
+export async function POST(req: Request, { params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
 
-export async function POST(req: Request, context: Context) {
-    const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: "No autenticado" }, { status: 401 });
+  }
 
-    if(!session) return NextResponse.json({error: "No autenticado"}, {status: 401});
+  if (session.user.role !== "MAESTRO") {
+    return NextResponse.json(
+      { error: "No autorizado para eliminar una criatura" },
+      { status: 403 }
+    );
+  }
 
-    if(session.user.role !== "MAESTRO") return NextResponse.json({error: "No autorizado para eliminar una criatura"}, {status: 403});
+  const creatureId = params.id;
 
-    const creatureId = context.params.id;
+  try {
+    await prisma.creature.delete({
+      where: { id: creatureId },
+    });
 
-    try {
-        await prisma.creature.delete({
-            where: {id: creatureId},
-        });
-
-        return NextResponse.redirect(new URL("/criaturas", req.url));
-    } catch (error) {
-        console.error("Error al elimianr la criatura:", error);
-        return NextResponse.json({error: "Error al eliminar"}, {status: 500});
-    }
+    return NextResponse.redirect(new URL("/criaturas", req.url));
+  } catch (error) {
+    console.error("Error al eliminar la criatura:", error);
+    return NextResponse.json({ error: "Error al eliminar" }, { status: 500 });
+  }
 }
